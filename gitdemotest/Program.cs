b9c9a6d6501e6604ab4d08d5726b041b543503a3
Mark Surrow <mark.surrow@s-d.dk>
2020-11-11 12:47:46 +0100

    Created project

---
+++ gitdemotest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace gitdemotest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person p = new Person() { FirstName = "Anders", LastName = "And" };
+            Console.Write(p);
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override String ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
